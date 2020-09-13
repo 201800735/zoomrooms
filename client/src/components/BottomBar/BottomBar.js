e3ae9433bac20a82efdab8df79bd444af1877bfd
Lee Chimfwembe <49544669+201800735@users.noreply.github.com>
2020-09-13 02:20:54 +0200

    Add files via upload

---
+++ client/src/components/BottomBar/BottomBar.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BottomBar = ({
+  clickChat,
+  goToBack,
+  toggleCameraAudio,
+  userVideoAudio,
+  clickScreenSharing,
+  screenShare,
+}) => {
+  return (
+    <Bar>
+      <Left>
+        <CameraButton onClick={toggleCameraAudio} data-switch="video">
+          <div>
+            {userVideoAudio.video ? (
+              <FaIcon className="fas fa-video"></FaIcon>
+            ) : (
+              <FaIcon className="fas fa-video-slash"></FaIcon>
+            )}
+          </div>
+          Camera
+        </CameraButton>
+        <CameraButton onClick={toggleCameraAudio} data-switch="audio">
+          <div>
+            {userVideoAudio.audio ? (
+              <FaIcon className="fas fa-microphone"></FaIcon>
+            ) : (
+              <FaIcon className="fas fa-microphone-slash"></FaIcon>
+            )}
+          </div>
+          Audio
+        </CameraButton>
+      </Left>
+      <Center>
+        <ChatButton onClick={clickChat}>
+          <div>
+            <FaIcon className="fas fa-comments"></FaIcon>
+          </div>
+          Chat
+        </ChatButton>
+        <ScreenButton onClick={clickScreenSharing}>
+          <div>
+            <FaIcon
+              className={`fas fa-desktop ${screenShare ? 'sharing' : ''}`}
+            ></FaIcon>
+          </div>
+          Share Screen
+        </ScreenButton>
+      </Center>
+      <Right>
+        <StopButton onClick={goToBack}>Stop</StopButton>
+      </Right>
+    </Bar>
+  );
+};
+
+const Bar = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+  background-color: #4ea1d3;
+`;
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-left: 15px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+const Right = styled.div``;
+
+const ChatButton = styled.div`
+  width: 75px;
+  border: none;
+  font-size: 0.9375rem;
+  padding: 5px;
+
+  :hover {
+    background-color: #77b7dd;
+    cursor: pointer;
+    border-radius: 15px;
+  }
+
+  * {
+    pointer-events: none;
+  }
+`;
+
+const ScreenButton = styled.div`
+  width: auto;
+  border: none;
+  font-size: 0.9375rem;
+  padding: 5px;
+
+  :hover {
+    background-color: #77b7dd;
+    cursor: pointer;
+    border-radius: 15px;
+  }
+
+  .sharing {
+    color: #ee2560;
+  }
+`;
+
+const FaIcon = styled.i`
+  width: 30px;
+  font-size: calc(16px + 1vmin);
+`;
+
+const StopButton = styled.div`
+  width: 75px;
+  height: 30px;
+  border: none;
+  font-size: 0.9375rem;
+  line-height: 30px;
+  margin-right: 15px;
+  background-color: #ee2560;
+  border-radius: 15px;
+
+  :hover {
+    background-color: #f25483;
+    cursor: pointer;
+  }
+`;
+
+const CameraButton = styled.div`
+  width: 75px;
+  border: none;
+  font-size: 0.9375rem;
+  padding: 5px;
+
+  :hover {
+    background-color: #77b7dd;
+    cursor: pointer;
+    border-radius: 15px;
+  }
+
+  * {
+    pointer-events: none;
+  }
+
+  .fa-microphone-slash {
+    color: #ee2560;
+  }
+
+  .fa-video-slash {
+    color: #ee2560;
+  }
+`;
+
+export default BottomBar;
